c2f52fb1dff1a89d30055f224ed726404b6b8120
Ospinajuanp <juanpo996@gmail.com>
2023-02-06 21:18:20 -0500

    reset partida

--- src/containers/GameBoard.jsx
+++ src/containers/GameBoard.jsx
@@ -1,13 +1,33 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import AppContext from '../context/AppContext'
 import BoxTap from '../components/BoxTap';
 import '../styles/GameBoard.scss'
 
 const GameBoard = () => {
-    const { state } = useContext(AppContext);
-    const a = () => {
+    const { state, resetBoard, togglePlayerPlay, resetGame } = useContext(AppContext);
+    const [ stateReset, SetStateReset ] = useState(false)
+    const [ stateResetGame, SetStateResetGame ] = useState(false)
+    const resetThisGame = () => {
+        SetStateReset(true)
+        resetBoard()
+    }
+    const resetAllGame = () => {
+        stateResetGame(true)
+        resetGame()
         console.log(state)
     }
+    useEffect(()=>{
+        if (stateReset){
+            let x = ((state.amountWinPlayerOne+state.amountWinPlayerTwo)%2) ? 2:1
+            togglePlayerPlay(x)
+            SetStateReset(false)
+        }
+        if(stateResetGame){
+            resetGame()
+            SetStateResetGame(false)
+        }
+    })
+
 
     return (
         <div className='GameBoard'>
@@ -26,7 +46,8 @@ const GameBoard = () => {
                 )
             }
             </div>
-            <div onClick={a} className='GameBoard-reset'>Reset</div>
+            <div id='reset' onClick={resetThisGame} className='GameBoard-reset'>Reset Progress</div>
+            <div id='reset' onClick={resetAllGame} className='GameBoard-reset'>Reset Game</div>
         </div>
     );
 };
--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -98,8 +98,10 @@ const useInitialState = () => {
 			boardState:state.boardState
 		});
 	};
-	const togglePlayerPlay = () => {
-		state.playerPlay = state.playerPlay==1 ? 2:1
+	const togglePlayerPlay = (player) => {
+		state.playerPlay = state.playerPlay==1 ? 2:1		
+		if (player) state.playerPlay = player
+		console.log(player)
 		setState({
 			...state,
 			playerPlay:state.playerPlay
@@ -169,6 +171,70 @@ const useInitialState = () => {
 			]
 		})
 	}
+	const resetGame = () =>{
+		playerPlay:1
+		amountWinPlayerOne:0
+		amountWinPlayerTwo:0
+		boardState:[
+		[// row
+			{ id: 1, value: false, player:null },// column
+			{ id: 2, value: false, player:null },
+			{ id: 3, value: false, player:null },
+			{ id: 4, value: false, player:null },
+			{ id: 5, value: false, player:null },
+			{ id: 6, value: false, player:null }
+		],
+		[// row
+			{ id: 7, value: false, player:null },// column
+			{ id: 8, value: false, player:null },
+			{ id: 9, value: false, player:null },
+			{ id: 10, value: false, player:null },
+			{ id: 11, value: false, player:null },
+			{ id: 12, value: false, player:null }
+		],
+		[// row
+			{ id: 13, value: false, player:null },// column
+			{ id: 14, value: false, player:null },
+			{ id: 15, value: false, player:null },
+			{ id: 16, value: false, player:null },
+			{ id: 17, value: false, player:null },
+			{ id: 18, value: false, player:null }
+		],
+		[// row
+			{ id: 19, value: false, player:null },// column
+			{ id: 20, value: false, player:null },
+			{ id: 21, value: false, player:null },
+			{ id: 22, value: false, player:null },
+			{ id: 23, value: false, player:null },
+			{ id: 24, value: false, player:null }
+		],
+		[// row
+			{ id: 25, value: false, player:null },// column
+			{ id: 26, value: false, player:null },
+			{ id: 27, value: false, player:null },
+			{ id: 28, value: false, player:null },
+			{ id: 29, value: false, player:null },
+			{ id: 30, value: false, player:null }
+		],
+		[// row
+			{ id: 31, value: false, player:null },// column
+			{ id: 32, value: false, player:null },
+			{ id: 33, value: false, player:null },
+			{ id: 34, value: false, player:null },
+			{ id: 35, value: false, player:null },
+			{ id: 36, value: false, player:null }
+		],
+		[// row
+			{ id: 37, value: false, player:null },// column
+			{ id: 38, value: false, player:null },
+			{ id: 39, value: false, player:null },
+			{ id: 40, value: false, player:null },
+			{ id: 41, value: false, player:null },
+			{ id: 42, value: false, player:null }
+		],
+
+	]
+	}
 
 	return {
 		state,
@@ -176,7 +242,8 @@ const useInitialState = () => {
 		falseCardsState,
 		togglePlayerPlay,
 		sumAmountWinPlayer,
-		resetBoard
+		resetBoard,
+		resetGame
 	}
 }
 
